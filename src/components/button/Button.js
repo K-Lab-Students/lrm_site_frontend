6c659cfea412b27667e3af5f53808d1e10ab8b19
Vadim <akimovvadim779@gmail.com>
2022-02-22 20:30:15 +0300

    Реализация кнопки

---
+++ src/components/button/Button.js
@@ -0,0 +1,6 @@
+import style from './style';
+import './stylecss.css';
+
+const Button = (props) => <button className='Button'> {props.text} </button>
+
+export default Button;
---
+++ src/components/button/style.js
@@ -0,0 +1,33 @@
+const style = {
+  form: {
+      margin: 50,
+      padding: 10,
+      width: 300,
+      border: "1px solid black",
+      backgroundColor: "black",
+      color: "white"
+  },
+  inputGroup: {
+      marginBottom: 10
+  },
+  input: {
+      backgroundColor: "#EFEFFF",
+      marginLeft: 10
+  },
+  error: {
+      color: "red",
+      margin: 5
+  },
+  myButton: {
+    backgroundColor: 'green', /* Green */
+    border: 'none',
+    color: 'white',
+    padding: 15,
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: 16,
+    width: 300,
+    height: 300
+  }
+};
