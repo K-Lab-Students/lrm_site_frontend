91d68fd076354e956db44fbd90772b9d4bbe3228
Vadim <akimovvadim779@gmail.com>
2022-02-26 14:14:46 +0300

    Реализация NavBar с помощью bootstrap

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,57 @@
 import './style.css'
-import { Nav } from 'react-bootstrap'
+import { Nav, Navbar, Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Header = () => {
     return (
-        <Nav defaultActiveKey="/home" as="ul" className='Header'>
-            <Nav.Item as="li">
-                <Nav.Link href="/home"><span style={{color: 'white'}}>Active</span></Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link eventKey="link-1"><span style={{color: 'white'}}>other</span></Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link eventKey="link-2"><span style={{color: 'white'}}>oky</span></Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <div className='Header'>
+            <div className='HeaderContentDiv'>
+                <Nav defaultActiveKey="/home" as="ul" className='Container'>
+                    <Nav defaultActiveKey="/home" as="ul" className='Container'>
+                        <Nav.Item as="li">
+                            <Nav.Link href="/home">
+                                <img src="https://img.icons8.com/office/30/000000/brain.png"/>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+
+                    <Nav defaultActiveKey="/home" as="ul" className='Container'>
+                        <Nav.Item as="li">
+                            <Nav.Link href="/home">
+                                <span style={{color: 'white'}} className='ContainerItemNav'>Active</span>
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item as="li">
+                            <Nav.Link eventKey="link-1">
+                                <span style={{color: 'white'}} className='ContainerItemNav'>other</span>
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item as="li">
+                            <Nav.Link eventKey="link-2">
+                                <span style={{color: 'white'}} className='ContainerItemNav'>oky</span>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+
+                    <Nav defaultActiveKey="/home" as="ul" className='Container'>
+                        <Nav.Item as="li">
+                            <Nav.Link href="/home">
+                                <span style={{color: 'white'}} className='ContainerItemNav'>Sing In</span>
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item as="li">
+                            <Nav.Link eventKey="link-1">
+                                <span style={{color: 'white'}} className='ContainerItemNav'>Sing Up</span>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+
+                </Nav>
+            </div>
+        </div>
     )
 }
 
