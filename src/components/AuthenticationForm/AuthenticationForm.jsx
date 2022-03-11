9a5c106f00c2d9043e538876417bcca17a450837
Vadim <akimovvadim779@gmail.com>
2022-03-11 20:05:47 +0300

    Добавлена форма авторизации пользователя

--- src/components/AuthenticationForm/AuthenticationForm.jsx
+++ src/components/AuthenticationForm/AuthenticationForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Text, TextInput, PasswordInput, Button, Container } from '@mantine/core'
+
 import Body from '../Body/Body'
 
 import styles from './AuthenticationFormStyle.module.css'
@@ -7,11 +9,14 @@ import styles from './AuthenticationFormStyle.module.css'
 const AuthenticationForm = ({ width }) => {
     return (
         <Body width={width}>
-            <center>
-                <div style={{ verticalAlign: 'middle' }}>
-                    Вход
+            <Container className={styles.authContainer}>
+                <div className={styles.authDiv}>
+                    <Text>Вход</Text>
+                    <TextInput label='Логин' />
+                    <PasswordInput label='Пароль' />
+                    <Button>Вход</Button>
                 </div>
-            </center>
+            </Container>
         </Body>
     )
 }
