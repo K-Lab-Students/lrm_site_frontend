28241923f2839bff5aea2c2397320ff356869307
Vadim <akimovvadim779@gmail.com>
2022-04-07 18:16:32 +0300

    Добавлен удобный редактор текста. Обновлен вывод новостей.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Text, Divider, Group } from '@mantine/core'
+import { Container, Text, Divider, Group, TypographyStylesProvider } from '@mantine/core'
 
 import styles from './PostStyle.module.css'
 
@@ -11,26 +11,15 @@ const Post = ({ postData }) => {
     const baseUrl = NewsService.host
 
     return (
-        // <Container className={styles.post}>
-        //     <Group >
-        //         <Text size='xl'>{postData.name}</Text>
-        //         {/* {postData.userId.map(tagName => <Badge color='pink'>{tagName}</Badge>)} */}
-
-        //     </Group>
-        //     <Divider />
-        //     <Text>{postData.body}</Text>
-        // </Container>
-
         <div className={[styles.card]}>
-            <div className={styles.cardHeader}>
+            {/* <div className={styles.cardHeader}>
                 {postData.name}
-            </div>
+            </div> */}
             <div className={styles.cardBody}>
                 <Group grow>
-                    <img src={baseUrl + postData.files[0].file_path} width={200} alt='some' />
-                    <div style={{textAlign: 'center'}}>
-                        {postData.body}
-                    </div>
+                    <TypographyStylesProvider>
+                        <div dangerouslySetInnerHTML={{ __html: postData.body }} />
+                    </TypographyStylesProvider>
                 </Group>
             </div>
         </div>
